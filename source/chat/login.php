d543990dd4dd78b49ae83a5728f41a202f117a87
fkampas03 <fkampas@student.tgm.ac.at>
2021-03-18 14:35:42 +0100

    Oberfläche von Login und Register fertig

--- source/chat/login.php
+++ source/chat/login.php
@@ -4,9 +4,56 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--Importiert das Bootstrap css-File-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <!--Importiert das css-File-->
+    <link rel="stylesheet" href="style/styleLogin.css">
+
+    <!--Importiert das Bootstrap script-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous" async></script>
+    
+
     <title>Login</title>
 </head>
 <body>
-    
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <div class="col-11 ms-5">
+                    <a class="navbar-brand nav-font" href="#">Chat</a>
+                </div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav ">
+                        <a class="nav-link active" aria-current="page" href="register.php">Register</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <fieldset class="col-lg-6 col-md-8 col-sm-10 col-xs-11 mx-auto mt-5 py-4 px-5 fieldsetStyle">
+            <legend>Login</legend>
+            <form name="formLogin" action="php/process.php" method="POST" enctype="multipart/form-data">
+                <div class="mb-3">
+                    <label for="loginName" class="form-label">Username or Email</label>
+                    <input type="text" class="form-control" id="loginName" name="loginName" required>
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="password" name="password" minlength="8" required>
+                </div>
+                <div class="mb-3">
+                    <button type="submit" class="btn btn-primary" name="submit">Register</button>
+                </div>
+                <div class="mb-2">
+                    <a href="register.php" class="link">Havent't got an account yet? Register here!</a>
+                </div>
+            </form>
+        </fieldset>
+    </main>
 </body>
 </html>
--- source/chat/register.php
+++ source/chat/register.php
@@ -18,25 +18,44 @@
 </head>
 <body>
     <header>
-
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <div class="col-11 ms-5">
+                    <a class="navbar-brand nav-font" href="#">Chat</a>
+                </div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav ">
+                        <a class="nav-link active" aria-current="page" href="login.php">Login</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
     </header>
     <main>
         <fieldset class="col-lg-6 col-md-8 col-sm-10 col-xs-11 mx-auto mt-5 py-4 px-5 fieldsetStyle">
             <legend>Register</legend>
-            <form name="form" action="php/process.php" method="POST" enctype="multipart/form-data">
+            <form name="formRegister" action="php/process.php" method="POST" enctype="multipart/form-data">
                 <div class="mb-3">
-                    <label for="benutzername" class="form-label">Benutzername</label>
-                    <input type="text" class="form-control" id="benutzername" name="benutzername" required>
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" class="form-control" id="username" name="username" required>
                 </div>
                 <div class="mb-3">
                     <label for="email" class="form-label">Email</label>
                     <input type="email" class="form-control" id="email" name="email" required>
                 </div>
                 <div class="mb-3">
-                    <label for="password" class="form-label">Passwort</label>
+                    <label for="password" class="form-label">Password</label>
                     <input type="password" class="form-control" id="password" name="password" minlength="8" required>
                 </div>
-                <button type="submit" class="btn btn-primary" name="submit">Register</button>
+                <div class="mb-3">
+                    <button type="submit" class="btn btn-primary" name="submit">Register</button>
+                </div>
+                <div class="mb-2">
+                    <a href="login.php" class="link">Already got an account? Login here!</a>
+                </div>
             </form>
         </fieldset>
     </main>
